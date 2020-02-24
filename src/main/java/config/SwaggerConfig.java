911244ce7e625f6346735688e165f142119c1a8b
lapengo <mylapengo@gmail.com>
2020-02-24 11:16:20 +0700

    Structure App

---
+++ src/main/java/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package config;
+
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author ARIF LAPENGO
+ *
+ */
+@EnableSwagger2
+public class SwaggerConfig {
+	@Bean
+	public Docket customImplementation(){
+	      return new Docket(DocumentationType.SWAGGER_2)
+	    	  .apiInfo(getApiInfo())	  
+	    	  .select()
+		      .apis(RequestHandlerSelectors.basePackage("controller"))
+		      .paths(PathSelectors.any())
+		      .build();
+		}
+		
+		private ApiInfo getApiInfo() {
+		   return new ApiInfo("REST Api Documentation",
+				      "REST Api Documentation", 
+				      "1.0", 
+				      "urn:tos",
+				      new Contact("Chubb", "", ""), 
+			          "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	   }
+}
